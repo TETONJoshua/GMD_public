22bc1716011a3e85178559d3953faa6baadeb9c1
Romain Defranoux <romain.defranoux@telecomnancy.eu>
2023-05-11 22:19:59 +0200

    parsing xml ok

---
+++ GMD/Mapping/RecordDrugBankXML.cs
@@ -0,0 +1,30 @@
+﻿namespace GMD.Mapping
+{
+    public class RecordDrugBankXML
+    {
+        public string atcCode { get; set; } 
+        public string name { get; set; }
+
+        public string interaction { get; set; }
+        public string toxicity { get; set; }
+        public List<string> synonyms { get; set; }
+        public List<string> products { get; set; }
+
+        public RecordDrugBankXML(string atcCode, string name, string interaction, string toxicity, List<string> synonyms, List<string> products )
+        {
+            this.atcCode = atcCode;
+            this.name = name;
+            this.interaction = interaction;
+            this.synonyms = synonyms;
+            this.products = products;
+        }
+        public RecordDrugBankXML()
+        {
+            this.name = "";
+            this.interaction = "";
+            this.toxicity = "";
+            this.synonyms = new List<string>();
+            this.products = new List<string>();
+        }
+    }
+}
--- GMD/Pages/Index.cshtml.cs
+++ GMD/Pages/Index.cshtml.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GMD.Mapping;
+using GMD.Services;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel;
 
 namespace GMD.Pages
 {
@@ -12,9 +16,20 @@ namespace GMD.Pages
             _logger = logger;
         }
 
+        public List<RecordDrugBankXML> recordDrugs { get; set; }
+        public int countNullATC, countRecord, countNullNames, countNullTox;
+        public long parseTime;
         public void OnGet()
         {
-
+            drugBankXML dbx = new drugBankXML();
+            Stopwatch sw = Stopwatch.StartNew();
+            recordDrugs = dbx.parseXML(); 
+            sw.Stop();
+            parseTime = sw.ElapsedMilliseconds;
+            countNullATC = dbx.countNullATC;
+            countRecord = dbx.countRecord;
+            countNullNames = dbx.countNullNames;
+            countNullTox = dbx.countNullTox;
         }
     }
 }
